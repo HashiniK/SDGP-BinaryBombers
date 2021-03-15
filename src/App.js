9dd43b307f8832e93a426c21c3d0512a30036f7a
MaleeshaDiluminda <61634241+MaleeshaDiluminda@users.noreply.github.com>
2021-03-15 23:31:18 +0530

    Update App.js
    <Route path = "/">
                  <Header />
                  <Home />
                </Route>
    
    Changed This part

--- src/App.js
+++ src/App.js
@@ -37,7 +37,7 @@ function  App() {
             </Route>
   
             <Route path = "/">
-              <Header />
+              <SearchPage/>
               <Home />
             </Route>
           </Switch>
