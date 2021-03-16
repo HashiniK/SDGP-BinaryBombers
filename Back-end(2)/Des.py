d8a6d4ad4d011c52e4f6bed106d783a13b320155
Deunisehara <66510866+Deunisehara@users.noreply.github.com>
2021-03-16 09:59:38 +0530

    Add files via upload

---
+++ Back-end(2)/Des.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+from flask import jsonify
+import pandas as pd
+
+@app.route('/binary-bomber-service/sampleData/product_desc', methods = ['GET', 'POST'])
+def pGender():
+    if request.method == 'GET':
+        df = readCSV()
+        return jsonify(list(df['product_desc'].unique()))
+
+def readCSV():
+    return pd.read_csv('SampleData-Mobitel.csv')
+
+if __name__ == '__main__':
+    app.run(host="localhost", port= 9000) #Enter your server IP here
---
+++ Back-end(2)/README.txt
@@ -0,0 +1,8 @@
+01. Install Flask
+    pip install Flask
+
+Implemented API (GET method) =>
+    http://localhost:9000/binary-bomber-service/sampleData/age
+    http://localhost:9000/binary-bomber-service/sampleData/package
+    http://localhost:9000/binary-bomber-service/sampleData/product_desc
+    http://localhost:9900/binary-bomber-service/customer
