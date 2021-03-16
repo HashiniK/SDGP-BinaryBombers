51f077dffb96e6d5388ba26f723069444c597f42
disurig <80665573+disurig@users.noreply.github.com>
2021-03-16 09:59:29 +0530

    Add files via upload

---
+++ backend1/BinaryBomberServer.py
@@ -0,0 +1,16 @@
+from flask import Flask, request
+app = Flask(__name__)
+from flask import jsonify
+import pandas as pd
+
+@app.route('/binary-bomber-service/sampleData/package', methods = ['GET', 'POST'])
+def HandleSampleData():
+    if request.method == 'GET':
+        df = readCSV();
+        return jsonify(list(df['package'].unique()))
+
+def readCSV():
+    return pd.read_csv('SampleData-Mobitel.csv')
+
+if __name__ == '__main__':
+    app.run(host="localhost", port= 9000) #Enter your server IP here
