62305fd0432f6c44741cfc838d8139bd8d8c1c54
HashiniK <66357863+HashiniK@users.noreply.github.com>
2021-03-23 00:14:44 +0530

    Updated SearchPage.js
    the content in the SearchPage was moved to SearchForm.js

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,85 +1,29 @@
 import React from 'react'
-import './SearchPage.css';
-import Header from './Header';
-import Footer from "./Footer";
+import {Component} from 'react'
+import SearchForm from './SearchForm'
 
-function SearchPage() {
-    return (
-       
-        <div  className="searchPage">
-             <Header/>
-            <div className="searchPage__box">
+class SearchPage extends Component{
+    state={
+        fields:{}
+    };
+    
+    onChange = updatedValue => {
+        this.setState({
+            fields: {
+               ...this.state.fields,
+               ...updatedValue 
+            }
+        });
+    };
 
-                {/* Search By Customer Box */}
-
-                <div className="searchPage__box1">
-
-                
-                <div className="searchPage__imageBox">
-                    <img 
-                        className="searchPage__images"
-                        src="2.gif"
-                        alt="Search By Customer Image"
-                    />
-                </div>
-                <strong>Search By Customer</strong>
-                <br></br>
-                {/* Search By Customer Box - Search Bar Input */}
-
-                <input type="text" className="searchPage__searchInput" />
-                
-                <light className="lightFont">Enter Customer ID</light>
-                <button className="search__Button">Search</button>
-                </div>
-
-                                
-                {/* Search By Package Box  */}
-
-                <div className="searchPage__box2">
-
-                
-                <div className="searchPage__imageBox">
-                    <img 
-                        className="searchPage__images"
-                        src="1.gif"
-                        alt="Search By Customer Image"
-                    />
-                </div>
-                <strong>Search By Package</strong>
-                {/* Search By Package Box - Search Bar Input */}
-                <br></br>
-                <input type="text" className="searchPage__searchInput" />
-            
-                <light className="lightFont">Enter Package ID</light>
-                <button className="search__Button" >Search</button>
-                </div>
-
-                {/* Search By N.B.O Box */}
-
-                <div className="searchPage__box3">
-                
-                <div className="searchPage__imageBox">
-                    <img 
-                        className="searchPage__images"
-                        src="3.gif"
-                        alt="Search By Customer Image"
-                    />
-                </div>
-                <strong>Search By N.B.O</strong>
-                <br></br>
-                {/* Search By N.B.O Box - Search Bar Input */}
-
-                <input type="text" className="searchPage__searchInput" />
-                <light className="lightFont">Enter Package ID</light>
-                <button className="search__Button">Search</button>
-                </div>
-
-                {/*  */}
-
+    render(){
+        return(
+            <div className="searchPage">
+                <SearchForm/>
             </div>
-            <Footer />
-        </div>
-        
-    )
+        );
+    }
 }
-export default SearchPage
+
+export default SearchPage;
+
